7818f0a7acf237fe2a519e22cb495657951394ea
YablokovaValeriya <99179279+YablokovaValeriya@users.noreply.github.com>
2022-06-10 22:46:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,7 +3,7 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
-#include  "tree.h"В
+#include  "tree.h"
 
 std::vector<char> getPerm(const Tree& tree, int n) {
   return Tree(tree).getCombinationsByNumber(tree, n);
