b5317753248df1d3c5acc9b4f14848f023c5f0fa
YablokovaValeriya <99179279+YablokovaValeriya@users.noreply.github.com>
2022-06-10 22:15:49 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,50 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+struct Node {
+  char symb;
+  Node *parent;
+  std::vector<Node> option;
+};
+
+class Tree {
+ private:
+  Node *root = new Node;
+  std::vector<Node> combinations;
+  void makeStructureTree(std::vector<char> array) {
+    makeNode(root, array);
+    conlink(root);
+  }
+  void makeNode(Node* crown, std::vector<char> array) {
+    for (int j = 0; j < array.size(); j++) {
+      Node *elem = new Node();
+      elem->symb = array[j];
+      crown->option.push_back(*elem);
+      if (array.size() > 0) {
+        makeNode(&crown->option[j], requireArray(array, array[j]));
+      }
+    }
+  }
+  void conlink(Node *crown) {
+    for (Node &child : crown->option) {
+      child.parent = crown;
+      conlink(&child);
+    }
+    if (crown->option.size() == 0) {
+      combinations.push_back(*crown);
+    }
+  }
+  std::vector<char> requireArray(std::vector<char> array, char value) {
+    std::vector<char>::iterator position = std::find(array.begin(),
+    array.end(), value);
+    if (position != array.end())
+      array.erase(position);
+      return array;
+  }
+
+ public:
+  explicit Tree(std::vector<char> array) {
+    makeStructureTree(array);
+  }
+};
 #endif  // INCLUDE_TREE_H_
