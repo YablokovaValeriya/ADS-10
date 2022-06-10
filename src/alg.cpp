be9e9a4441d34d98b06f5c4281417cd55d3c4803
YablokovaValeriya <99179279+YablokovaValeriya@users.noreply.github.com>
2022-06-10 22:18:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,21 @@
 #include  <cstdlib>
 #include  "tree.h"
 
-std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+std::vector<char> getCombinationsByNumber(Tree tree, int number){
+  if (number > combinations.size()) {
+    return {};
+  }
+  std::vector<char> result;
+  Node current = combinations[number-1];
+  while (current.parent) {
+    result.push_back(current.symb);
+    current = *current.parent;
+  }
+  std::reverse(result.begin(), result.end());
+  return result;
 }
+
+std::vector<char> getPerm(const Tree& tree, int n) {
+  return Tree(tree).getCombinationsByNumber(tree, n);
+}
+
